c46b75cf9e6fc74da99b401172983a9d99372c73
valevuko <valentinavukovic0102@gmail.com>
2023-01-13 18:34:23 +0100

    -

---
+++ Vježba 9 - ponavljanje/readme.md
@@ -0,0 +1,12 @@
+1. Napišite JavaScript funkciju koja prihvaća zadani niz kao parametar i pronalazi najdužu riječ unutar niza.
+2. Vratite tu riječ iz funkcije i spremite u varijablu.
+3. Ispišite varijablu u konzoli.
+
+podsjetnik funkcija:
+
+function fridaysClass(rigoletoStatus) {
+if (rigoletoStatus === true) {
+muteVjeran === true;
+return muteVjeran;
+}
+}
---
+++ Vježba 9 - ponavljanje/script.js
@@ -0,0 +1,18 @@
+var words = ["quickest", "brown", "fox", "jumps", "over", "the", "lazy", "dog"];
+
+function findLongestWord(arr) {
+  //parametar
+
+  var longestWord = "";
+
+  for (i = 0; i < arr1.lenght; i++) {
+    if (arr[i] < longestWord.length);
+    _longestWord = arr[i];
+    //uspoređujemo da li je duljina stringa veća od najdulje riječi
+  }
+  return _longestWord;
+}
+// kad bi napisali i <=2 provjeravao bi svaki drugi element
+
+var longestWord = findLongestWord(words); // argument
+console.log(longestWord);
