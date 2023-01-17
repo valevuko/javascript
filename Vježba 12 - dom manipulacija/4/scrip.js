23ec3873787bc3ad4c9910843276215263f8887c
valevuko <valentinavukovic0102@gmail.com>
2023-01-17 21:24:46 +0100

    -

---
+++ Vježba 12 - dom manipulacija/4/scrip.js
@@ -0,0 +1,22 @@
+var inputNode = document.querySelector("input");
+
+function handleBlur() {
+  console.log("blurred");
+}
+
+/* inputNode.addEventListener("blur", handleBlur);
+ */
+/* inputNode.addEventListener("focus", function () {
+ console.log("focused");
+}); */
+
+inputNode.addEventListener("keyup", function (event) {
+  console.log("Input clicked");
+  console.log(event.target.value);
+});
+
+/* var divNode = document.querySelector("div");
+divNode.addEventListener("clik", function () {
+  console.log("Div clicked");
+});
+ */
